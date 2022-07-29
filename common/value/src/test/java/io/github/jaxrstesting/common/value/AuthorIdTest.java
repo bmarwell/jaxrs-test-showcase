0615cf4b7ef769d0d8930010921f7b1bc9ff8953
Benjamin Marwell <bmarwell@apache.org>
2022-07-29 08:49:11 +0200

    [TEST] add some unit tests.

---
+++ common/value/src/test/java/io/github/jaxrstesting/common/value/AuthorIdTest.java
@@ -0,0 +1,27 @@
+package io.github.jaxrstesting.common.value;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+class AuthorIdTest {
+
+  @SuppressWarnings("ConstantConditions")
+  @Test
+  void author_id_has_fromString_method() {
+    // given
+    var authorIdString = "rpfeynman";
+
+    // when
+    var authorIdFromString = AuthorId.fromString(authorIdString);
+    var authorId = new AuthorId(authorIdString);
+
+    // then
+    assertThat(authorIdFromString)
+        .isEqualTo(authorId)
+        .matches(aidc -> aidc instanceof ValueWrapper<String>);
+  }
+}
